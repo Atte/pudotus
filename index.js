8a91a41fb7546b7ceccbcc3f5e3871b28e0d55e9
Atte Virtanen <aten.email@gmail.com>
2023-08-26 15:00:28 +0300

    cleanup

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const QUERY = 'fmi::forecast::harmonie::hybrid::point::simple';
-const SURFACE_QUERY = 'fmi::forecast::harmonie::surface::point::simple';
+const HEIGHTS = [4000, 3500, 3000, 2500, 2000, 1500, 1000, 600, 300, 0];
 const PARAMS = {
     Temperature: 'Temperature',
     WindDirection: 'WindDirection',
     WindSpeedMS: 'WindSpeedMS',
 };
-const HEIGHTS = [4000, 3500, 3000, 2500, 2000, 1500, 1000, 600, 300, 0];
 
-const MAIN = document.getElementById('main');
-const STATUS = document.getElementById('status');
-const STATUS_CONTAINER = document.getElementById('status-container');
-
-/** @type {HTMLTableRowElement} */
-const ROW_TEMPLATE = document.getElementById('row-template').content.querySelector('tr');
-
+/**
+ * @param {[number|string, number|string]} latlon
+ */
 async function getData(latlon, height) {
     const now = new Date();
     const time = `${now.getUTCFullYear()}-${now.getUTCMonth() + 1}-${now.getUTCDate()}T${now.getUTCHours()}:00:00Z`;
 
-    const query = height == 0 ? SURFACE_QUERY : QUERY;
-    const params = Object.values(PARAMS).join(',');
+    const url = new URL('https://opendata.fmi.fi/wfs');
+    url.searchParams.set('request', 'GetFeature');
+    url.searchParams.set('parameters', Object.values(PARAMS).join(','));
+    url.searchParams.set('latlon', latlon.join(','));
+    url.searchParams.set('starttime', time);
+    url.searchParams.set('endtime', time);
 
-    const resp = await fetch(
-        `https://opendata.fmi.fi/wfs?request=GetFeature&storedquery_id=${query}&parameters=${params}&latlon=${latlon}&height=${height}&starttime=${time}&endtime=${time}`,
-        {
-            mode: 'cors',
-            credentials: 'omit',
-            referrerPolicy: 'no-referrer',
-        }
-    );
+    if (height == 0) {
+        url.searchParams.set('storedquery_id', 'fmi::forecast::harmonie::surface::point::simple');
+    } else {
+        url.searchParams.set('storedquery_id', 'fmi::forecast::harmonie::hybrid::point::simple');
+        url.searchParams.set('height', height);
+    }
+
+    const resp = await fetch(url, {
+        mode: 'cors',
+        credentials: 'omit',
+        referrerPolicy: 'no-referrer',
+    });
     const dom = new DOMParser().parseFromString(await resp.text(), 'text/xml');
 
     const data = {};
@@ -42,27 +44,33 @@ async function getData(latlon, height) {
 }
 
 async function reload_impl() {
-    STATUS.textContent = 'Geolocating';
-    STATUS_CONTAINER.classList.remove('hidden');
-    MAIN.innerHTML = '';
+    /** @type {HTMLTableRowElement} */
+    const rowTemplate = document.getElementById('row-template').content.querySelector('tr');
+    const main = document.getElementById('main');
+    const status = document.getElementById('status');
+    const statusContainer = document.getElementById('status-container');
+
+    status.textContent = 'Geolocating';
+    statusContainer.classList.remove('hidden');
+    main.innerHTML = '';
 
     /** @type {GeolocationPosition} */
     const position = await new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
     const latlon = [position.coords.latitude.toFixed(2), position.coords.longitude.toFixed(2)];
 
     for (const height of HEIGHTS) {
-        STATUS.textContent = `Loading ${height}m`;
+        status.textContent = `Loading ${height}m`;
         const data = await getData(latlon, height);
 
-        const tr = ROW_TEMPLATE.cloneNode(true);
+        const tr = rowTemplate.cloneNode(true);
         tr.querySelector('.height').textContent = height.toLocaleString('fi');
         tr.querySelector('.temperature').textContent = data[PARAMS.Temperature].toFixed(0);
         tr.querySelector('.wind-speed').textContent = data[PARAMS.WindSpeedMS].toFixed(0);
         tr.querySelector('.wind-direction .arrow').setAttribute('style', `--direction: ${data[PARAMS.WindDirection]}deg`);
-        MAIN.appendChild(tr);
+        main.appendChild(tr);
     }
 
-    STATUS_CONTAINER.classList.add('hidden');
+    statusContainer.classList.add('hidden');
 }
 
 function reload() {
