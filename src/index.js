ca374588396cc293984ea0a2edc0deece74b555f
Atte Virtanen <aten.email@gmail.com>
2023-08-31 21:23:23 +0300

    Switch to XML based API to reduce number of requests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const HEIGHTS = [4000, 3500, 3000, 2500, 2000, 1500, 1000, 600, 300, 0];
 const PARAMS = {
+    GeomHeight: 'GeomHeight',
     Temperature: 'Temperature',
     WindDirection: 'WindDirection',
     WindSpeedMS: 'WindSpeedMS',
@@ -15,6 +16,28 @@ const CLOUD_HEIGHTS = {
 };
 const MINUTES_ACCURACY = 5;
 
+const REQUEST_TEMPLATE = `<?xml version="1.0" ?>
+<wfs:GetFeature
+    service="WFS"
+    version="2.0.2"
+    outputFormat="application/gml+xml; version=3.2"
+    xmlns:wfs="http://www.opengis.net/wfs/2.0"
+    xmlns:fes="http://www.opengis.net/fes/2.0"
+    xmlns:gml="http://www.opengis.net/gml/3.2"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.opengis.net/wfs/2.0 http://schemas.opengis.net/wfs/2.0/wfs.xsd">
+    <wfs:StoredQuery>
+        <wfs:Parameter name="latlon">
+            <gml:pos></gml:pos>
+        </wfs:Parameter>
+        <wfs:Parameter name="height"></wfs:Parameter>
+        <wfs:Parameter name="starttime"></wfs:Parameter>
+        <wfs:Parameter name="endtime"></wfs:Parameter>
+        <wfs:Parameter name="timestep"></wfs:Parameter>
+        <wfs:Parameter name="parameters"></wfs:Parameter>
+    </wfs:StoredQuery>
+</wfs:GetFeature>`;
+
 /** @returns {string} */
 function getDataTime() {
     const now = new Date();
@@ -29,29 +52,36 @@ function getDataTime() {
 /**
  * @param {string} time
  * @param {[number|string, number|string]} latlon
- * @param {number} height
  * @param {AbortController?} abortController
- * @returns {Promise<{[key: string]: number}>}
+ * @returns {Promise<{[height: number]: {[key: string]: number}}>}
  */
-async function getData(time, latlon, height, abortController) {
-    const url = new URL('https://opendata.fmi.fi/wfs');
-    url.searchParams.set('request', 'GetFeature');
-    url.searchParams.set('latlon', latlon.join(','));
-    url.searchParams.set('starttime', time);
-    url.searchParams.set('endtime', time);
-    url.searchParams.set('timestep', 1);
+async function getData(time, latlon, abortController) {
+    /** @type {XMLDocument} */
+    const request = new DOMParser().parseFromString(REQUEST_TEMPLATE, 'text/xml');
+    const queryTemplate = request.querySelector('StoredQuery');
+    queryTemplate.remove();
 
-    if (height === 0) {
-        url.searchParams.set('storedquery_id', 'fmi::forecast::harmonie::surface::point::simple');
-        url.searchParams.set('parameters', Object.values(PARAMS).concat(Object.values(SURFACE_PARAMS)).join(','));
-    } else {
-        url.searchParams.set('storedquery_id', 'fmi::forecast::harmonie::hybrid::point::simple');
-        url.searchParams.set('parameters', Object.values(PARAMS).join(','));
-        url.searchParams.set('height', height);
+    for (const height of HEIGHTS) {
+        const query = queryTemplate.cloneNode(true);
+        query.querySelector('[name="latlon"] > pos').textContent = latlon.join(' ');
+        query.querySelector('[name="starttime"]').textContent = time;
+        query.querySelector('[name="endtime"]').textContent = time;
+        query.querySelector('[name="timestep"]').textContent = MINUTES_ACCURACY;
+        if (height === 0) {
+            query.id = 'fmi::forecast::harmonie::surface::point::simple';
+            query.querySelector('[name="parameters"]').textContent = Object.values(PARAMS).concat(Object.values(SURFACE_PARAMS)).join(' ');
+        } else {
+            query.id = 'fmi::forecast::harmonie::hybrid::point::simple';
+            query.querySelector('[name="parameters"]').textContent = Object.values(PARAMS).join(' ');
+            query.querySelector('[name="height"]').textContent = height;
+        }
+        request.firstChild.appendChild(query);
     }
 
-    url.searchParams.sort();
-    const response = await fetch(url, {
+    const response = await fetch('https://opendata.fmi.fi/wfs', {
+        method: 'POST',
+        body: new XMLSerializer().serializeToString(request),
+        headers: { 'Content-Type': 'text/xml' },
         mode: 'cors',
         credentials: 'omit',
         signal: abortController?.signal,
@@ -61,15 +91,18 @@ async function getData(time, latlon, height, abortController) {
     }
 
     const dom = new DOMParser().parseFromString(await response.text(), 'text/xml');
-
-    const data = {};
-    for (const el of dom.querySelectorAll('member > BsWfsElement')) {
-        const value = parseFloat(el.querySelector('ParameterValue').textContent);
-        if (Number.isFinite(value)) {
-            data[el.querySelector('ParameterName').textContent] = value;
+    const datas = {};
+    for (const collection of dom.querySelectorAll('member > FeatureCollection')) {
+        const data = {};
+        for (const element of collection.querySelectorAll('member > BsWfsElement')) {
+            const value = parseFloat(element.querySelector('ParameterValue').textContent);
+            if (Number.isFinite(value)) {
+                data[element.querySelector('ParameterName').textContent] = value;
+            }
         }
+        datas[data[PARAMS.GeomHeight] ?? 0] = data;
     }
-    return data;
+    return datas;
 }
 
 /** @type {AbortController|null} */
@@ -107,19 +140,18 @@ async function refresh() {
     }
     const latlon = [position.coords.latitude.toFixed(2), position.coords.longitude.toFixed(2)];
 
+    status.textContent = `Loading data`;
     const main = document.getElementById('main');
     const tbody = main.cloneNode(false);
 
     const updateTime = getDataTime();
-    let data;
+    const datas = await getData(updateTime, latlon, myAbortController);
+    if (myAbortController.signal.aborted) {
+        return;
+    }
+
     for (const height of HEIGHTS) {
-        status.textContent = `Loading ${height}m`;
-
-        data = await getData(updateTime, latlon, height, myAbortController);
-        if (myAbortController.signal.aborted) {
-            return;
-        }
-
+        const data = datas[height];
         const tr = rowTemplate.cloneNode(true);
         tr.querySelector('tr').dataset.height = height;
         tr.querySelector('.height').textContent = height.toLocaleString('fi');
@@ -132,12 +164,12 @@ async function refresh() {
 
     for (const [height, param] of Object.entries(CLOUD_HEIGHTS)) {
         const td = cloudCoverTemplate.cloneNode(true);
-        td.querySelector('.value').textContent = data[param].toFixed(0);
+        td.querySelector('.value').textContent = datas[0][param].toFixed(0);
         tbody.querySelector(`tr[data-height="${height}"]`).appendChild(td);
     }
 
     const td = windGustTemplate.cloneNode(true);
-    td.querySelector('.wind-gust').textContent = data[SURFACE_PARAMS.WindGust].toFixed(0);
+    td.querySelector('.wind-gust').textContent = datas[0][SURFACE_PARAMS.WindGust].toFixed(0);
     tbody.querySelector(`tr[data-height="0"]`).appendChild(td);
 
     if (abortController === myAbortController) {
